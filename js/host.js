fa0068b731b46d1a4d171e627baa4b53d4276a4f
BavonChao <19404655+BavonChao@users.noreply.github.com>
2019-01-26 13:34:38 +0800

    Update host.js

--- js/host.js
+++ js/host.js
@@ -2,7 +2,6 @@ var host = "http://47.110.63.69";
 
 //步骤一:创建异步对象
 var ajax = new XMLHttpRequest();
-window.location=host + '/api/get_csrf_token/' + document.cookie;
 //步骤二:设置请求的url参数,参数一是请求的类型,参数二是请求的url,可以带参数,动态的传递参数starName到服务端
 ajax.open('get',host + '/api/get_csrf_token/');
 //步骤三:发送请求
